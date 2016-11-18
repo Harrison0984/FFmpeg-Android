b3fee4bea7812506958319782375c0612b6897ee
heyunpeng <heyunpeng@heyunpengdeMacBook-Pro.local>
2016-11-18 17:27:21 +0800

    first commit

---
+++ build_android_armeabi.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#Change NDK to your Android NDK location
+PLATFORM=$ANDROID_NDK/platforms/android-18/arch-arm/
+PREBUILT=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64
+
+GENERAL="\
+--enable-small \
+--enable-cross-compile \
+--extra-libs="-lgcc" \
+--arch=arm \
+--cc=$PREBUILT/bin/arm-linux-androideabi-gcc \
+--cross-prefix=$PREBUILT/bin/arm-linux-androideabi- \
+--nm=$PREBUILT/bin/arm-linux-androideabi-nm \
+--extra-cflags="-I../x264-android/android/arm/include" \
+--extra-ldflags="-L../x264-android/android/arm/lib" "
+
+
+MODULES="\
+--enable-gpl \
+--enable-libx264"
+
+function build_ARMv6
+{
+  ./configure \
+  --target-os=linux \
+  --prefix=./android/armeabi \
+  ${GENERAL} \
+  --sysroot=$PLATFORM \
+  --enable-shared \
+  --enable-static \
+  --extra-cflags=" -O3 -fpic -fasm -Wno-psabi -fno-short-enums -fno-strict-aliasing -finline-limit=300 -mfloat-abi=softfp -mfpu=vfp -marm -march=armv6" \
+  --extra-ldflags="-lx264 -Wl,-rpath-link=$PLATFORM/usr/lib -L$PLATFORM/usr/lib -nostdlib -lc -lm -ldl -llog" \
+  --enable-zlib \
+  ${MODULES} \
+  --disable-doc \
+  --enable-neon
+
+  make clean
+  make
+  make install
+}
+
+
+
+build_ARMv6
+
+echo Android ARMEABI builds finished
